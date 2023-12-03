3c1c02ba8aaea18478f1f370daa64e767fc4fbe5
KarolKucinski2001 <101826493+KarolKucinski2001@users.noreply.github.com>
2023-12-03 22:51:02 +0100

    .

--- Main.py
+++ Main.py
@@ -6,18 +6,9 @@ def main():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
 
-    # Przykładowe pobranie danych do data table dla różnych kryptowalut
-    # cryptos = ['BTC', 'ETH', 'BNB', 'SOL']
-    # for crypto in cryptos:
-        # url = Functions.construct_download_url(crypto, '2022-11-27', '2023-11-27', 'weekly')
-        # data = Functions.scrape_yahoo_finance_data(url, headers)
-        # Analysis.changing_format(data)
-        # Analysis.endoftheday_data_weekly(data, crypto)
-        # Analysis.endoftheday_vs_beginningoftheday_data_weekly(data,crypto)
-    
     # Funkcja pobierająca dane i analizująca dla wybranej kryptowaluty
     def analyze_crypto(crypto):
-        url = Functions.construct_download_url(crypto, '2022-11-27', '2023-11-27', 'daily')
+        url = Functions.construct_download_url(crypto, '2022-11-27', '2023-11-27', 'weekly') #ostatni parametr moze być weekly
         data = Functions.scrape_yahoo_finance_data(url, headers)
         print(f'Analiza danych dla {crypto}:\n{data}')
         Analysis.plot_candlestick_chart(data,crypto)
@@ -32,6 +23,6 @@ def main():
     analyze_crypto('ETH')
     Analysis.profit4Crypto()
 
-    
+
 if __name__ == "__main__":
     main()
