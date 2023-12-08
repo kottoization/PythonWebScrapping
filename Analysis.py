e46bc916310e367400f64fc4904e2afbcca8a8c2
Mateusz Mulka <96475096+kottoization@users.noreply.github.com>
2023-12-08 23:40:05 +0100

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,8 +1,6 @@
 import Functions 
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas as pd
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
